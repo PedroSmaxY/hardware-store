9b0238b4e6a750e5647b4fe125fd6730f9f8e752
PedroSmaxY <pedrohnovais@outlook.com>
2025-06-16 13:47:02 -0300

    Adicionar arquivo servicos_relatorio.py com descrição dos serviços de relatórios

---
+++ src/servicos/servicos_relatorio.py
@@ -0,0 +1,8 @@
+"""
+Este arquivo implementa os serviços de relatórios para o sistema de loja de hardware,
+fornecendo funcionalidades de consulta e análise de dados. Centraliza a geração
+de relatórios de vendas, estoque, clientes e performance, aplicando filtros
+e cálculos específicos para apoiar a tomada de decisões gerenciais.
+"""
+
+# TODO: Implementar os serviços de relatórios com as consultas e análises necessárias
