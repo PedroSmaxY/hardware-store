d283313eb6dc5219d64de47a762a322fe5462f5e
PedroSmaxY <pedrohnovais@outlook.com>
2025-06-16 15:44:33 -0300

    Remover comentário de tipo desnecessário na importação de sqlalchemy_utils

--- src/configs/config_bd.py
+++ src/configs/config_bd.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, create_database  # type: ignore
+from sqlalchemy_utils import database_exists, create_database
 from src.configs.config_globais import URL_BANCO_DE_DADOS
 
 """
