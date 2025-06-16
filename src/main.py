2c84835a2d5241cf55399e668d86fd2f3c0d78a8
PedroSmaxY <pedrohnovais@outlook.com>
2025-06-16 17:57:19 -0300

    Implementar inicialização da aplicação com PyQt6 no código principal

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import sys
+from PyQt6.QtWidgets import QApplication
 from src.configs.config_bd import iniciar_bd
 
 # TODO: Implementar o restante do código principal do sistema de loja de hardware
@@ -5,4 +7,7 @@ from src.configs.config_bd import iniciar_bd
 
 def main():
     iniciar_bd()
-    # Restante do código principal do sistema de loja de hardware ...
+
+    app = QApplication(sys.argv)
+
+    sys.exit(app.exec())
